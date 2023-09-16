df922faa4f329a3b2bbcb42bb3e7ec1dff79a861
simplysabir <simplysabir@gmail.com>
2023-09-16 10:37:49 +0530

    fixed navbar and search bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
   }
   return (
     <div className="flex w-full justify-center flex-col items-center mt-20">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex w-full justify-center flex-col items-center">
       <div className="flex w-full max-w-sm items-center space-x-2">
         <Input
           type="email"
